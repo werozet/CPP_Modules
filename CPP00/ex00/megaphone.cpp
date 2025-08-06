1bf716732935ed52f27d3896e11dff85c51f5b62
Wero <159571299+werozet@users.noreply.github.com>
2025-08-06 18:57:58 +0200

    Add files via upload

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzielins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/06 17:48:25 by wzielins          #+#    #+#             */
+/*   Updated: 2025/08/06 17:48:26 by wzielins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+			{
+				std::cout << (char)toupper(av[i][j]);
+			}
+			if (i < ac - 1)
+				std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
